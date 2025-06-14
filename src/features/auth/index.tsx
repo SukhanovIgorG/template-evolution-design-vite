968718806b94a7287ccc66187a2b042824ec431b
SukhanovIgorG <sukhanovgarik@yandex.ru>
2025-06-14 13:53:20 +0300

    feat: add evolution structure

---
+++ src/features/auth/index.tsx
@@ -0,0 +1,15 @@
+export const LoginPage = () => {
+  return (
+    <div>
+      <h1>login page</h1>
+    </div>
+  );
+};
+
+export const RegisterPage = () => {
+  return (
+    <div>
+      <h1>register page</h1>
+    </div>
+  );
+};
